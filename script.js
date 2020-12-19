7d8f59b715bbafc940daf34401f1b5802b6e9951
jdcar <jdcarlstone@gmail.com>
2020-12-18 19:50:19 -0600

    added photo random and track search desc

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
 
 document.cookie = "Set-Cookie: SameSite=None; Secure"
 
+// Points to JSON file of genres
 var mydata = data
+
+// adds these to global variable
 var selectedId = ""
 var input = ""
 var trackName = ""
-var selectedRecords = "5"
+// makes the selected default 5
+var selectedRecords = 5
+
+// Event listener for genre drop down 
 $("#genre-list").change(function () {
     // console.log(this.value)
     selectedId = this.value
 });
 
-$("num-records").change(function(){
+// Event listener for number of results drop down
+$("#num-records").change(function () {
     selectedRecords = this.value
+    console.log(selectedRecords)
+
 })
 
+// Submit button event listener
 $("#submit").click(function (event) {
-    input = $('input').val()
-    // console.log(input)
+    event.preventDefault()
 
+    // Grabs the city name from input
+    input = $('input').val()
+
+    // Searches for an image based on city input
     var imgUrl = `https://pixabay.com/api/?key=19570887-9ab24a9c116677c7d4d20872b&q=${input}&image_type=photo`
     $.ajax({
         url: imgUrl,
         method: "GET"
     }).then(function (img) {
-        console.log(img)
-        var imgCode = img.hits[0].largeImageURL
-        console.log(imgCode)
+        var randomImg = img.hits // gets array 
+        var ran = Math.floor(Math.random() * randomImg.length) // randomizes length of array. outputs a number 
+        console.log(ran)
+        var newImage = img.hits[ran].largeImageURL //inputs number into array with image url
+        console.log(newImage)
+        //appending to doc 
         var imgDiv = $("<img>")
-        imgDiv.attr("src", imgCode)
-        // add image size
-        $(".img").append(imgDiv)
-    
+        imgDiv.attr("src", newImage)
+        $(".img").html(imgDiv)
+
     })
 
 
-    // 
+    // Adds a map to the DOM based on the URL
     mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvbWFuMDkiLCJhIjoiY2tpcGFzbm1nMGJkazJxbzFsOGg3dW5ueCJ9.nKYluwYKT0XqFDXaafIJTQ';
     // var city = input
     var queryUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=pk.eyJ1IjoiZHJvbWFuMDkiLCJhIjoiY2tpcGFzbm1nMGJkazJxbzFsOGg3dW5ueCJ9.nKYluwYKT0XqFDXaafIJTQ"
@@ -44,10 +59,10 @@ $("#submit").click(function (event) {
         url: queryUrl,
         method: "Get"
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
         var lng = response.features[0].center[0];
         var lat = response.features[0].center[1];
-        console.log(lng, lat)
+        // console.log(lng, lat)
 
 
         var map = new mapboxgl.Map({
@@ -56,24 +71,22 @@ $("#submit").click(function (event) {
             center: [lng, lat], // starting position [lng, lat]
             zoom: 8 // starting zoom
         });
-        console.log(map)
+        // console.log(map)
 
     })
 
 
-    
-
-
-    // 
-
-    // event.preventDefault()
     $.ajax({
         type: "GET",
         data: {
             apikey: "4d79a850d35d2e78003074885fedf889",
+            // Input is from the form
             q_lyrics: input,
+            // Selected from the genre drop down
             f_music_genre_id: selectedId,
+            // Selected from the number of records drop down
             page_size: selectedRecords,
+            s_track_rating: "desc",
             format: "jsonp",
             page: '1',
             callback: "jsonp_callback"
@@ -84,25 +97,26 @@ $("#submit").click(function (event) {
         contentType: 'application/json',
         success: function (data) {
             console.log(data);
+            console.log(selectedRecords)
 
-            if (data.message.body.track_list[0]) {
-                trackName = JSON.stringify(data.message.body.track_list[0].track.track_name)
-                artistName = JSON.stringify(data.message.body.track_list[0].track.artist_name)
-                lyricsUrl = JSON.stringify(data.message.body.track_list[0].track.track_share_url)
+            // Loops through the number selected for 
+            for (var i = 0; i < Number(selectedRecords); i++) {
+
+                // console.log(data.message.body.track_list[i])
+                // Gets track name, artist, URL of lyrics
+                trackName = JSON.stringify(data.message.body.track_list[i].track.track_name)
+                artistName = JSON.stringify(data.message.body.track_list[i].track.artist_name)
+                lyricsUrl = JSON.stringify(data.message.body.track_list[i].track.track_share_url)
 
                 trackNameParse = JSON.parse(trackName)
                 artistNameParse = JSON.parse(artistName)
                 lyricsParse = JSON.parse(lyricsUrl)
 
-                // $('h2').append($(`<p>${trackNameParse}</p>`));
-            
-                $('h2').append($(`<p>${artistNameParse}</p>`));
-                $('h2').append($(`<a href=${lyricsParse} target="_blank">${trackNameParse}</a>`));
-
-                
+                // Appends the artist, URL, track name to DOM
+                $('h4').append($(`<p>${artistNameParse}</p>`));
+                $('h4').append($(`<a href=${lyricsParse} target="_blank">${trackNameParse}</a>`));
 
             }
-
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(jqXHR);
@@ -114,10 +128,11 @@ $("#submit").click(function (event) {
 
 })
 
+// Creates a drop down list of genres
 for (var i = 0; i < 360; i++) {
 
     if (mydata.message.body.music_genre_list[i].music_genre.music_genre_parent_id === 34) {
-        // console.log(mydata.message.body.music_genre_list[i].music_genre.music_genre_name)
+
 
         var genres = mydata.message.body.music_genre_list[i].music_genre.music_genre_name
 
@@ -161,15 +176,7 @@ for (var i = 0; i < 360; i++) {
 // "https://api.musixmatch.com/ws/1.1/music.genres.get"
 
 
-
-
-
-
-// Integrate the track-search and genre api on the term
-
-// Music add-ons
-    // Get song chords with songsterr
-        // http://www.songsterr.com/a/ra/songs.json?pattern=Marley
+// http://www.songsterr.com/a/ra/songs.json?pattern=Marley
 
 
 
